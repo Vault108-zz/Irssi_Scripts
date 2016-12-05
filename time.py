772c2612b7e6d2ae7147bf9abe91b483b279f746
Vault108 <tyr5678@gmail.com>
2016-12-04 19:51:58 -0800

    addd Time.py

--- time.py
+++ time.py
@@ -1,3 +1,7 @@
+## Replace America/New_York With your Timezone
+## They can be found here
+## https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
 from datetime import datetime 
 from pytz import timezone 
 us = timezone('America/New_York') 
