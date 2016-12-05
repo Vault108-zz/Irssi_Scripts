3fc88af646cae4164ba2e9c0d4d76843219c2f0d
Vault108 <tyr5678@gmail.com>
2016-12-04 23:22:35 -0800

    added Pokemon.py

---
+++ Pokemon.py
@@ -0,0 +1,11 @@
+## Generates a random Url for Pokemon Fusion. 
+
+## credit to Alex Onsager for creating Pokemon Fusion  
+
+import random
+
+a = (random.randint(1,151))
+
+b = (random.randint(1,151))
+
+print "pokemon.alexonsager.net/%s/%s" % (a, b)
