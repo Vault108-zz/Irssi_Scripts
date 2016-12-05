6ca2996b1cee6580f86de5f8fa310d12f0f1dbc0
Vault108 <tyr5678@gmail.com>
2016-12-04 23:48:54 -0800

    "

--- Time.py
+++ Time.py
@@ -1,9 +1,15 @@
 ## Replace America/New_York With your Timezone
+
 ## They can be found here
+
 ## https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
 from datetime import datetime 
+
 from pytz import timezone 
-us = timezone('America/New_York') 
-sa_time = datetime.now(us)
+
+zone  = timezone('America/New_York') 
+
+sa_time = datetime.now(zone)
+
 print sa_time.strftime("Today is %B %d, %Y" " The Time is " "%I:%M%P")
